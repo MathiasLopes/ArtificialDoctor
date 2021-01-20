6e19230f242aaaa8e860479504159dc8b3a633c2
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-01-20 12:03:18 +0100

    [FEAT] Redirection page HTML

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const router = express.Router();
+var bodyParser = require('body-parser')
+var session = require('express-session');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var pathPublic = __dirname + "/public";
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//parse json
+app.use(bodyParser.json({limit: '50mb'}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+//session
+app.use(session({
+	secret:'sessionartificaldoctor',
+	resave: true,
+	saveUninitialized: true,
+}));
+
+app.use(express.static(pathPublic));
+
+router.get("/", function(req, res){
+	res.sendFile(path.join(pathPublic+'/webpages/index.html'));
+});
+
+router.get("/test", function(req, res){
+	res.sendFile(path.join(pathPublic+'/webpages/test.html'));
+});
+
+app.use('/', router);
+
+
+//laissé ces lignes après toutes les routes : 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +50,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.sendFile(path.join(pathPublic+'/webpages/notfound.html'));
 });
 
 module.exports = app;
