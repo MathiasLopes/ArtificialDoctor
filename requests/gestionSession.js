bb86852894a128f4d429db8858a48ff490b62e0f
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-01-25 21:37:31 +0100

    ajout de méthode de gestion de la session

---
+++ requests/gestionSession.js
@@ -0,0 +1,48 @@
+
+//permet de connecté l'utilisateur en session
+function createSession(req){
+
+    var result = {success: false, message: "KO"};
+
+    try{
+        
+		req.session.userconnected = true;
+
+      	result = {success: true, message: "Session créé"};
+
+    }catch(e){
+      	result = {success: false, message: e.message};
+    }
+
+    return result;
+
+}
+
+
+//permet de déconnecté l'utilisateur en session
+function removeSession(req){
+
+    var result = {success: false, message: "KO"};
+
+    try{
+        
+		req.session.userconnected = false;
+
+      	result = {success: true, message: "Token effacé"};
+
+    }catch(e){
+      	result = {success: false, message: e.message};
+    }
+
+    return result;
+}
+
+//permet de savoir si l'utilisateur est connecté ou non 
+function userIsConnected(req){
+    return req.session.userconnected;
+}
+
+
+exports.createSession = createSession;
+exports.removeSession = removeSession;
+exports.userIsConnected = userIsConnected;
