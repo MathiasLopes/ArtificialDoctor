493b9d194d86134df6aa1faef2ece8f35d63a892
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-01-25 21:35:14 +0100

    Ajout du blocage de l'accès direct au page /webpages + création de session avec GUID

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 const router = express.Router();
 var bodyParser = require('body-parser')
+const crypto = require("crypto");
 var session = require('express-session');
 var requestAuthentification = require('./requests/requestAuthentification');
 
@@ -16,12 +17,24 @@ app.use(bodyParser.json({limit: '50mb'}));
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
+console.log("je passe 1");
+
+//console.log(crypto.randomBytes(16).toString("hex"));
+
 //session
 app.use(session({
+  genid: function(req) {
+    return crypto.randomBytes(16).toString("hex") // use UUIDs for session IDs
+  },
 	secret:'sessionartificaldoctor',
 	resave: true,
 	saveUninitialized: true,
 }));
+
+console.log("je passe 2");
+
+//route pour toutes les pages de webpages, pour quel ne soit pas accessible
+app.get('/webpages/*', (req, res) => res.sendFile(path.join(pathPublic+'/webpages/notfound.html')));
 
 //chemin d'accès au dossier public
 app.use(express.static(pathPublic));
@@ -36,18 +49,24 @@ app.get('/login', (req, res) => res.sendFile(path.join(pathPublic+'/webpages/aut
 //route pour aller sur la page d'inscription
 app.get('/register', (req, res) => res.sendFile(path.join(pathPublic+'/webpages/authentification/register.html')));
 
+
 //constante contenant tous les utilisateurs 
 //(en temporaire serveur temps qu'on a pas fait l'enregistrement en base de données des infos)
 const users = {}
 
 //methode qui inscrit l'utilisateur
 app.post('/register', (req, res) => {
-  requestAuthentification.inscription(req, function(reponse){res.send(reponse);});
+  requestAuthentification.inscription(req, function(reponse){
+    res.send(reponse);
+  });
 })
 
 //methode qui permet de connecter l'utilisateur
 app.post('/login', async (req, res) => {
-  requestAuthentification.connexion(req, function(reponse){res.send(reponse);});
+  requestAuthentification.connexion(req, function(reponse){
+    console.log(reponse);
+    res.send(reponse);
+  });
 })
 
 /*router.get("/", function(req, res){
