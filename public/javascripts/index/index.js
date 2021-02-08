afd4c4da81e63aa22e3093a886ec20f1a7993a68
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-02-08 22:15:54 +0100

    [FEAT] ouverture des articles + affichage du loading pendant chargement des articles

--- public/javascripts/index/index.js
+++ public/javascripts/index/index.js
@@ -1,13 +1,17 @@
 
 function getArticles(data){
 
+    showLoading();
+
     call_newyorktime(data, function(result){
+        
+        hideLoading();
         
         var htmlArticles = "";
 
         for(var i = 0; i < result.response.docs.length; i++){
             var unArticle = result.response.docs[i];
-            htmlArticles += `<div class="unArticle">
+            htmlArticles += `<div class="unArticle" onclick="openInNewTab('` + unArticle.web_url + `')">
                                 <div class="content-titre"><div class="titre">Titre :</div><div>` + unArticle.headline.main + `</div></div>
                                 <div class="content-description"><div class="description">Description :</div><div>` + unArticle.snippet + `</div></div>
                                 <div class="content-source"><div class="source">Source :</div><div>` + unArticle.source + `</div></div>
@@ -17,4 +21,9 @@ function getArticles(data){
         $("#list-articles").html(htmlArticles);
     });
 
+}
+
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
 }
