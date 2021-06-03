e5fe2ae2bc01bf8cd0e4eae4a54ae42d90ccb66c
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-06-03 13:35:09 +0200

    redirection des boutons + hide bt deconnexion pour cgu et rgpd

--- public/packages/header/header.js
+++ public/packages/header/header.js
@@ -4,15 +4,24 @@ fichier nécessaire au bon fonctionnement du header :
 - /packages/header/header.js
 - /packages/header/header.css
 - /javascripts/packages/jquery.min.js
-
+- /javacripts/app/table_utilisateurs.js
 */
 
 //code html du header
 var html_header = `<div class="title" onclick="goToIndex();">MyHealth <img class="iconeLogo" src="/packages/images/logo.png" alt="logo" /></div>
                    <div class="btLogout" onclick="deconnexion();">Se déconnecter</div>`;
+
+var userIsConnected = false;
+
 //permet d'inclure le code html du header dans la page au chargement
 $(document).ready(function(){
     $("#header").html(html_header);
+
+    getMe(function(data){
+        userIsConnected = data.success;
+        if(!userIsConnected)
+            $(".btLogout").hide();
+    });
 });
 
 function deconnexion(){
@@ -20,5 +29,8 @@ function deconnexion(){
 }
 
 function goToIndex(){
-    window.location.href = "/index";
+    if(userIsConnected)
+        window.location.href = "/index";
+    else
+        window.location.href = "/login";
 }
